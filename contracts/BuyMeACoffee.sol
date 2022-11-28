bbf30b9aa328423389b35cfc10ec09c4da4652bf
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-11-28 12:31:38 -0500

    first commit

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+contract BuyMeACoffee {
+    event NewMemo (
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+    
+    Memo [] memos;
+
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+}
