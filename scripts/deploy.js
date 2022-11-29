dcba43d4effbe9d4d81048513f29f7183d92fa0d
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-11-29 12:53:49 -0500

    done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,67 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
-async function getBalance(address) {
-  const balanceBigInt = await hre.waffle.provider.getBalance(address);
-  return hre.ethers.utils.formatEther(balanceBigInt);
-}
-async function printBalances(addresses) {
-  let idx = 0;
-  for (const address of addresses) {
-    console.log(`Address ${idx} balance:`, await getBalance(address));
-    idx++;
-  }
-}
-async function printMemos(memo) {
-  for (const memo of memos) {
-    const timestamp = memo.timestamp;
-    const tipper = memo.name;
-    const tipperAddress = memo.from;
-    const message = memo.message;
-    console.log(
-      `At${timestamp},${tipper},(${tipperAddress}) said: "${message}"`
-    );
-  }
-}
 async function main() {
-  const [owner, tipper, tipper2, tipper3] = await hre.ether.getsigner();
-
   const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
   const buyMeACoffee = await BuyMeACoffee.deploy();
   await buyMeACoffee.deployed();
-  console.log("BuyMeACOffee deployed to", buyMeACoffee.address);
-
-  const addresses = [owner.address, tipper.address, buyMeACoffee.address];
-  console.log("== start ==");
-  await printBalances(addresses);
-
-  const tip = { value: hre.ethers.utils.parseEther("1") };
-  await buyMeACoffee
-    .connect(tipper)
-    .BuyCoffee("Gursewak", "You're the best", tip);
-  await buyMeACoffee.connect(tipper).BuyCoffee("Akash", "Amazing Work", tip);
-  await buyMeACoffee.connect(tipper).BuyCoffee("Amrinder", "Lets go", tip);
-
-  console.log("== bought coffee ==");
-  await printBalances(addresses);
-
-  await buyMeACoffee.connect(owner).withDrawTips();
-
-  console.log("== withdrawTips ==");
-  await printBalances(addresses);
-
-  console.log("== memos ==");
-  const memos = await buyMeACoffee.getMemos();
-  printMemos(memos);
+  console.log("BuyMeACoffee deployed to", buyMeACoffee.address);
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
